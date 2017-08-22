91dc2ee19ac259155129b41e38ca305a2d753311
John Dowd <irishdowd10@gmail.com>
2017-08-22 15:42:24 -0700

    set up files and finish project

--- Texter/Startup.cs
+++ Texter/Startup.cs
@@ -16,6 +16,7 @@ namespace Texter
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,7 +29,14 @@ namespace Texter
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+
+			app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
